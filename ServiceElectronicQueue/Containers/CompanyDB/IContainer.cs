5d4ab9c2bd6a16e2dfa355297ac1d766b8d9ed95
artem <artem@Z0NDE>
2024-05-24 04:41:05 +0300

    Add ParserTransmittingDataContainer and edit sessions controllers

--- ServiceElectronicQueue/Containers/CompanyDB/IContainer.cs
+++
@@ -1,8 +0,0 @@
-﻿/*namespace ServiceElectronicQueue.Containers.CompanyDB;
-
-public interface IContainer<TView, TEntity> 
-    where TView : class 
-    where TEntity : class
-{
-    public void Expansion(ManagerContainer<TView, TEntity> container);
-}*/
