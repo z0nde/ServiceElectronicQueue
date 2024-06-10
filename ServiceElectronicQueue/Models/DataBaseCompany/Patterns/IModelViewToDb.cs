2e80b104b3d594f6ab641e53d29d3b557da7455f
artem <artem@Z0NDE>
2024-06-10 03:20:19 +0300

    Generate UniqueLink and Many changes by code

--- ServiceElectronicQueue/Models/DataBaseCompany/Patterns/IModelViewToDb.cs
+++
@@ -1,8 +0,0 @@
-﻿/*namespace ServiceElectronicQueue.Models.DataBaseCompany;
-
-public interface IModelViewToDb<T, T1> 
-    where T : class
-    where T1 : class
-{
-    public T ToDb(T1 view);
-}*/
