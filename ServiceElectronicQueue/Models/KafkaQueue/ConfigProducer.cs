b02b9862aca7cc45433868eadc9caa06d0a1a063
artem <artem@Z0NDE>
2024-06-17 17:54:20 +0300

    Fix Services, fix kafka, Big update

--- ServiceElectronicQueue/Models/KafkaQueue/ConfigProducer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ServiceElectronicQueue.Models.KafkaQueue;
-
-public class ConfigProducer
-{
-    public string ClientId { get; set; }
-    
-    public ConfigProducer(string id) => ClientId = id;
-}
